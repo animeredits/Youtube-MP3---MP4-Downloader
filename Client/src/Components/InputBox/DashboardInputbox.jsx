57c4b824e6a51eaa62b3e7227abd63646faa3c73
animeredits <rohitkmt63@gmail.com>
2024-06-15 01:28:45 +0530

    update

--- Client/src/Components/InputBox/DashboardInputbox.jsx
+++ Client/src/Components/InputBox/DashboardInputbox.jsx
@@ -46,7 +46,7 @@ const DashboardInputbox = () => {
     setError(""); // Reset error state
     setIsDownloadButtonDisabled(true);
     try {
-      const response = await fetch(`${import.meta.env.VITE_MP3}`, {
+      const response = await fetch(`${import.meta.env.VITE_MP3}/api/mp3`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
